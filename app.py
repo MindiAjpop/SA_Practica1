6de2441bc9c89733f2ad76b1694c578ce611262a
Mindi Ajpop <36805367+MindiAjpop@users.noreply.github.com>
2021-06-07 02:28:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return ""
+    return "<form action=\"\" method=\"get\"><p>Nombre: <input type=\"text\" name=\"nombre\" size=\"40\"></p><p>Año de nacimiento: <input type=\"number\" name=\"nacido\" min=\"1900\"></p><p>Sexo:<input type=\"radio\" name=\"hm\" value=\"h\"> Hombre<input type=\"radio\" name=\"hm\" value=\"m\"> Mujer</p><p><input type=\"submit\" value=\"Enviar\"><input type=\"reset\" value=\"Borrar\"></p></form>"
